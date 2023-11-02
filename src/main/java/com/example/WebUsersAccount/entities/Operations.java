366d22611f3cf8d37acef6d50e3786d18ae6202b
Andrew Zola <and.zola.77@gmail.com>
2023-11-02 20:22:44 +0100

    Commit with Operations(works)manyToOne with @Getters

--- src/main/java/com/example/WebUsersAccount/entities/Operations.java
+++ src/main/java/com/example/WebUsersAccount/entities/Operations.java
@@ -2,15 +2,17 @@ package com.example.WebUsersAccount.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
-@Data
+
 @Entity
 @Component
 @Table(name = "list_of_operations")
 public class Operations {
 
+        @Getter
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id_operation")
@@ -20,12 +22,35 @@ public class Operations {
         @JoinColumn(name = "users_account_id")
         private Users users;
 
+        @Getter
         @Column(name = "type_operation")
         private String Type_operation;
 
+        @Getter
         @Column(name = "timeOfOperation")
         private LocalDateTime timeOfOperation;
 
+        @Getter
         @Column(name = "sum_operation")
         private int sumOfOperation;
+
+        public void setIdOperation(int idOperation) {
+                this.idOperation = idOperation;
+        }
+
+        public void setUsers(Users users) {
+                this.users = users;
+        }
+
+        public void setType_operation(String type_operation) {
+                Type_operation = type_operation;
+        }
+
+        public void setTimeOfOperation(LocalDateTime timeOfOperation) {
+                this.timeOfOperation = timeOfOperation;
+        }
+
+        public void setSumOfOperation(int sumOfOperation) {
+                this.sumOfOperation = sumOfOperation;
+        }
 }
